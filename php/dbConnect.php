41781f2f89c0c730a653960bdff5bbbd2fce45e0
CkwKahWeng <129114565+CkwKahWeng@users.noreply.github.com>
2024-11-13 15:09:29 +0800

    Push and merge to branch

--- php/dbConnect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$dbConnection = mysqli_connect("localhost","root","","bit216");
-
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: ". mysqli_connect_error();
-    exit();
-}
-
-?>
